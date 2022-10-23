cee7edc417377da692bac22da5e38f7245edd3d0
AlbaFlorez1 <114140227+AlbaFlorez1@users.noreply.github.com>
2022-10-23 02:37:51 -0500

    Add files via upload
    look and feel

---
+++ js/Myjs.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+window.onload=iniciar;
+
+function alerta(){
+window.alert("Estamos trabajando para usted...");
+}
+
+function mouseOver(){
+	document.getElementById("foto").style.color = "red";
+	}
+
+function mouseOut() {
+	document.getElementById("foto").style.color = "black";
+	}
